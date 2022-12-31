ee25cf15aa2e338da8af6392f81e590656d4d8f9
MaruthiKo <marurohi@gmail.com>
2022-12-31 07:49:37 +0530

    Translated text using Minions API

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const txtArea = document.querySelector("#textarea-input");
 const outputTxtArea = document.querySelector("#textarea-output");
 
 
+
+
 // translates text when button is pressed
 btn.addEventListener("click",() => {
     // console.log(txtArea.value);
     // outputTxtArea.innerText = "Let's try to translate";
     // console.log(outputTxtArea.value);
+    fetch(constructUrl(txtArea.value))
+    .then(response => response.json())
+    .then(json => outputTxtArea.innerText = json.contents.translated)
+    .catch(errorHandler);
 });
+
+let constructUrl = (text) => `https://api.funtranslations.com/translate/minion.json?text=${text}`
+
+let errorHandler = (error) => console.log("The server is down" + error)
+
+
+
+
+
+
 
 
 
